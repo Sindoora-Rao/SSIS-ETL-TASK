a55aa4187de32402b747e5afcfae1320846a766d
Sindoora-Rao <sindoorasrao.26@gmail.com>
2024-07-22 22:50:01 -0400

    Initial commit for  Database Creation Script
    -Script includes creation of Database, Schemas and Tables.

---
+++ Scripts/DatabaseCreation.sql
@@ -0,0 +1,59 @@
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = N'KoreAssignment_Sindoora_Rao')
+BEGIN
+    CREATE DATABASE [KoreAssignment_Sindoora_Rao];
+END
+GO
+
+USE [KoreAssignment_Sindoora_Rao]
+GO
+
+-- Check and create stg schema if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'stg')
+BEGIN
+    EXEC('CREATE SCHEMA stg');
+END
+GO
+
+-- Check and create prod schema if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = N'prod')
+BEGIN
+    EXEC('CREATE SCHEMA prod');
+END
+GO
+
+-- Check and create stg.Users table if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'stg.Users') AND type in (N'U'))
+BEGIN
+    CREATE TABLE stg.Users (
+        StgID INT IDENTITY(1,1) PRIMARY KEY,
+        UserID INT,
+        FullName NVARCHAR(255),
+        Age INT,
+        Email NVARCHAR(255),
+        RegistrationDate DATE,
+        LastLoginDate DATE,
+        PurchaseTotal FLOAT
+    );
+END
+GO
+
+-- Check and create prod.Users table if it does not exist
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'prod.Users') AND type in (N'U'))
+BEGIN
+    CREATE TABLE prod.Users (
+        ID INT IDENTITY(1,1) PRIMARY KEY,
+        UserID INT,
+        FullName NVARCHAR(255),
+        Age INT,
+        Email NVARCHAR(255),
+        RegistrationDate DATE,
+        LastLoginDate DATE,
+        PurchaseTotal FLOAT,
+        RecordLastUpdated DATETIME DEFAULT GETDATE()
+    );
+END
+GO
+
+SELECT * from prod.Users;
+
+SELECT * from stg.Users;
